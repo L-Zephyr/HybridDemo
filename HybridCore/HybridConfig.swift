0cb17c642f1d97183d85f16de5dcdd8a9cc6c64f
LZephyr <lzf.lzephyr@gmail.com>
2017-04-25 23:36:06 +0800

    删除无用的代码

--- HybridCore/HybridConfig.swift
+++ HybridCore/HybridConfig.swift
@@ -9,24 +9,17 @@
 import UIKit
 
 class HybridConfig {
-    /// 打包在bundle中包含离线资源的目录
-    static var webRoot: String? = nil
-    
-    /// 本地缓存目录,默认为 Applcation Support/HybridCache
-    static var cachePath: String = CacheManager.shared.cachePath {
-        didSet {
-            CacheManager.shared.cachePath = cachePath
-        }
-    }
-    
-    /// 服务器地址
-    static var serverAddress: String? = nil
-    
-    /// 缓存类型白名单
-    static var cacheTypeWhiteList: [String] = ["js", "css", "html", "png", "jpg", "jpeg", "webp"]
-    
-    /// 缓存类型黑名单
-    static var cacheTypeBlackList: [String] = []
+//    /// 打包在bundle中包含离线资源的目录
+//    static var webRoot: String? = nil
+//    
+//    /// 服务器地址
+//    static var serverAddress: String? = nil
+//    
+//    /// 缓存类型白名单
+//    static var cacheTypeWhiteList: [String] = ["js", "css", "html", "png", "jpg", "jpeg", "webp"]
+//    
+//    /// 缓存类型黑名单
+//    static var cacheTypeBlackList: [String] = []
     
     /// 资源包更新完毕立即生效, 默认为下次启动时生效
     static var updateWhenFinishDownload: Bool = false
